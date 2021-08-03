04dcfab47c5ec9c68c4caabda9ed380c9ff63888
Knut Waale <kwaale@github.com>
2021-08-03 20:09:28 -0300

    ruta create

--- api/src/controllers/countriesControllers/addCountries.js
+++ api/src/controllers/countriesControllers/addCountries.js
@@ -1,8 +1,29 @@
-
+const fetch = require('node-fetch');
+const { Country } = require('../../db');
 // BASE_URL + "/countries/add";
 const addCountries = async (req, res, next) => {
     console.log('ruta addCountries');
-    res.json({ message: 'Obteniendo todos los países' });
+    try {
+        const response = await fetch(`https://restcountries.eu/rest/v2/all`);
+        const data = await response.json();
+        const countries = data.slice(0, 2).map(country => {
+            console.log(country);
+            Country.findOrCreate({
+                where: {
+                    id: country.alpha3Code
+                }
+            }
+            );
+        });
+    
+    res.json(countries)
+    // res.send('countries')
+        
+    } catch (error) {
+        console.error(error);
+    }
+
+    
 }
 
 module.exports = addCountries;
