7490c858c561737b1977d2d5800e1eb90e607ddf
Knut Waale <kwaale@github.com>
2021-08-04 10:50:28 -0300

    Add Countries

--- api/src/controllers/countriesControllers/addCountries.js
+++ api/src/controllers/countriesControllers/addCountries.js
@@ -1,29 +1,65 @@
 const fetch = require('node-fetch');
 const { Country } = require('../../db');
-// BASE_URL + "/countries/add";
+/*
+Ruta Post BASE_URL + "/countries/add";
+Recibe por body un arra de paises, en su defecto los busca en la api: https://restcountries.eu/rest/v2/all
+Los carga a la DB
+body = [{
+    id,             //Obligatorio
+    name,           //Obligatorio
+    callingCodes,
+    capital,
+    region,
+    subregion,
+    population,
+    area,
+    timezones,
+    borders,
+    nativeName,
+    languages,
+    flag,
+}]
+
+*/
 const addCountries = async (req, res, next) => {
     console.log('ruta addCountries');
     try {
-        const response = await fetch(`https://restcountries.eu/rest/v2/all`);
-        const data = await response.json();
-        const countries = data.slice(0, 2).map(country => {
-            console.log(country);
-            Country.findOrCreate({
-                where: {
-                    id: country.alpha3Code
-                }
-            }
-            );
-        });
-    
-    res.json(countries)
-    // res.send('countries')
-        
+        if(req.body.length > 0){
+            const countries = req.body;
+            await cargaArrayDB(countries);
+            res.json({status: 'ok'});
+        }else{
+            const response = await fetch(`https://restcountries.eu/rest/v2/all`);
+            const countries = await response.json();
+            await cargaArrayDB(countries);
+            res.json({status: 'ok'});
+        }
     } catch (error) {
         console.error(error);
     }
-
-    
+};
+//Recibe un array de paises y carga la DB
+const cargaArrayDB = async (arr)=>{
+    arr.forEach(async country => {
+        await Country.findOrCreate({
+            where: {
+                id: country.alpha3Code
+            },
+            defaults: {
+                name: country.name,
+                callingCodes: country.callingCodes,
+                capital: country.capital,
+                region: country.region,
+                subregion: country.subregion,
+                population: country.population,
+                area: country.area,
+                timezones: country.timezones,
+                borders: country.borders,
+                nativeName: country.nativeName,
+                languages: country.languages.map(language => language.name),
+                flag: country.flag,
+            }
+        });
+    });
 }
-
 module.exports = addCountries;
